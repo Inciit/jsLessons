72bdd41d1f8900fd31585aba4d7828760afef41c
incvvt <incit2202@yandex.ru>
2022-10-01 12:35:19 +0500

    Create first lessons stable project

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-a = 15;
+"use srtict" 
+
+const numberOfFilms = +prompt('Сколько фильмоы вы уже посмотрели?', '');
+
+console.log(numberOfFilms);
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {
+
+  },
+  actors: {
+
+  },
+  genres: [],
+  privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+  b = prompt('Насколько оцените его?', ''), 
+  c = prompt('Один из последних просмотренных фильмов ?', ''),
+  d = prompt('Насколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
